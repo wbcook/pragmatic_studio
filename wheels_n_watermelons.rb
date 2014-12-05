5f52285d8ea9a6d064f012125d66e8e1b246a31d
William Cook <wbcook@users.noreply.github.com>
2014-12-05 13:46:09 -0500

    Create wheels_n_watermelons.rb

---
+++ wheels_n_watermelons.rb
@@ -0,0 +1,10 @@
+# A fundraising app that tracks the funding of various odd projects.
+cattle = "Project Cattle"
+cattle_funding = 1_000
+
+puts "#{ cattle } has $#{ cattle_funding} in funding."
+
+wheels = "Project Wheels"
+watermelons = "Project Watermelons"
+
+puts "Projects:\n\t#{ cattle }\n\t#{ wheels }\n\t#{ watermelons }"
