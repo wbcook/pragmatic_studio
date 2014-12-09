85de509cd593c0efd3183812c998b0766782baff
William Cook <wbcook@users.noreply.github.com>
2014-12-08 22:37:42 -0500

    Update wheels_n_watermelons.rb

--- wheels_n_watermelons.rb
+++ wheels_n_watermelons.rb
@@ -1,16 +1,18 @@
 # A fundraising app that tracks the funding of various odd projects.
-cattle = "cattle"
-cattle_funds = 1000
-wheels = "wheels"
-wheels_funds = 525
-watermelons = "watermelons"
-watermelons_funds = 1250
-alpacas = "alpacas"
-alpacas_funds = 750
+def get_time
+  time = Time.new
+  time.strftime("%I:%M:%S")
+end
 
-print "Projects".center(20, '.')
-puts "Funding".center(20, '.')
-puts "#{ cattle.capitalize } has funding of #{ cattle_funds }."
-puts "#{ wheels.upcase } has funding of #{ wheels_funds }."
-puts "#{ watermelons.capitalize } has funding of #{ watermelons_funds }.".center(45, '*')
-puts "#{ alpacas.capitalize.ljust(25, '.') } has funding of #{ alpacas_funds }."
+def show_project( project, funds )
+  "*#{ project.capitalize }".ljust(20, '.') +
+  "$#{ funds }".ljust(20, '.') +
+  "#{get_time}*"
+end
+
+puts "Projects".center(49, '*')
+puts show_project( "wheels", 550 )
+puts show_project( "watermelons", 725)
+puts show_project( "cows", 425 )
+puts show_project( "alpacas", 875 )
+puts "Loans/Funds".center(49, '*')
