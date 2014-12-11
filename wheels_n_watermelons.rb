449e8b83391b003f49990fd54e20a978838cb3e5
William Cook <wbcook@users.noreply.github.com>
2014-12-11 00:01:38 -0500

    Update wheels_n_watermelons.rb

--- wheels_n_watermelons.rb
+++ wheels_n_watermelons.rb
@@ -1,18 +1,32 @@
 # A fundraising app that tracks the funding of various odd projects.
-def get_time
-  time = Time.new
-  time.strftime("%I:%M:%S")
+class Project
+  def initialize(name="project", funds=0, expire=30)
+    @name, @funds, @expire = name.capitalize, funds, expire
+  end
+  def to_s
+    "#{ @name } has $#{ @funds } and expires in #{ @expire } days."
+  end
+  def loan
+    @funds += 25
+    puts "Someone loaned $25 to #{@name}!!"
+  end
+  def expirer
+    @expire -= 1
+    puts "#{@name} expires soon, make a loan today!!" if @expire < 5
+  end
 end
 
-def show_project( project, funds )
-  "*#{ project.capitalize }".ljust(20, '.') +
-  "$#{ funds }".ljust(20, '.') +
-  "#{get_time}*"
-end
-
-puts "Projects".center(49, '*')
-puts show_project( "wheels", 550 )
-puts show_project( "watermelons", 725)
-puts show_project( "cows", 425 )
-puts show_project( "alpacas", 875 )
-puts "Loans/Funds".center(49, '*')
+wheels = Project.new( "wheels" )
+watermelons = Project.new( "watermelons", 725, 5)
+cows = Project.new( "cows", 425 )
+alpacas = Project.new( "alpacas", 875 )
+puts "Welcome".center(45, '*')
+wheels.loan
+wheels.loan
+watermelons.expirer
+puts "Projects".center(45, '*')
+puts wheels
+puts watermelons
+puts cows
+puts alpacas
+puts "Fundraising".center(45, '*')
